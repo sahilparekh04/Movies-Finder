5a5cb90e7f908dec701adb896383f97c3427eec0
Sahil Parekh <sahilparekh07@outlook.com>
2025-12-24 18:08:09 +0530

    Deployment

--- app.js
+++ app.js
@@ -1,124 +1,147 @@
-const cards = document.querySelector(".cards");
-let form = document.querySelector("#user-input");
-let search = document.querySelector("#search");
+const themeBtn = document.querySelector(".theme-btn");
+const icon = document.querySelector(".fa-regular");
+const html = document.documentElement;
+const themeValue = localStorage.getItem("theme");
+
+if (themeValue == "light") {
+  html.classList.remove("dark");
+  icon.classList.add("fa-moon");
+  icon.classList.remove("fa-sun");
+} else {
+  html.classList.add("dark");
+  icon.classList.add("fa-sun");
+  icon.classList.remove("fa-moon");
+}
+
+themeBtn.addEventListener("click", (e) => {
+  html.classList.toggle("dark");
+  if (html.getAttribute("class") == "light") {
+    localStorage.setItem("theme", "light");
+    icon.classList.add("fa-moon");
+    icon.classList.remove("fa-sun");
+  } else {
+    localStorage.setItem("theme", "dark");
+    icon.classList.add("fa-sun");
+    icon.classList.remove("fa-moon");
+  }
+});
+
+// Api Call
+
+async function moviesRequest(categoires, page, title) {
+  try {
+    const key = "78f34ce1";
+    const res = await fetch(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${key}&t=${title}&s=${categoires}&page=${page}`
+    );
+    const result = await res.json();
+    return result.Search;
+  } catch (error) {
+    console.error("There is issue while api call");
+  }
+}
+
+async function loadTrending(categoires, page) {
+  const trending = await moviesRequest(categoires, page);
+  const container = document.querySelector(".trending-card-container");
+
+  const newArr = trending.map((arr) => {
+    return `<div class="md:w-36 w-28 h-32 md:h-40 m-2.5 rounded-lg overflow-hidden relative">
+            <img
+              src="${arr.Poster}"
+            />
+            <div class="absolute bottom-0 left-0 bg-zinc-950 w-full p-1.5">
+              <h1 class="text-white font-medium text-sm md:text-lg mx-1.5">
+                ${arr.Title}
+              </h1>
+            </div>
+          </div>
+    `;
+  });
+
+  newArr.forEach((elem) => {
+    const div = document.createElement("div");
+    div.innerHTML = elem;
+    container.append(div);
+  });
+}
+
+async function loadPopular(categoires, page) {
+  const trending = await moviesRequest(categoires, page);
+  const container = document.querySelector(".popular-searches");
+
+  const newArr = trending.map((arr) => {
+    return `<div
+            class="md:w-40 w-28 p-2.5 m-2.5 rounded-lg hover:bg-neutral-800 hover:text-white transition-all duration-300 dark:text-white text-black"
+          >
+            <img
+              src="${arr.Poster}"
+              class="rounded-lg md:h-40 md:w-40"
+            />
+            <div class="mt-1 text-sm">
+              <h1 class="p-0.5 md:text-lg text-sm font-medium">${arr.Title}</h1>
+              <h5 class="p-0.5 md:text-sm text-xs font-medium">Year : ${arr.Year}</h5>
+            </div>
+          </div>`;
+  });
+
+  newArr.forEach((elem) => {
+    const div = document.createElement("div");
+    div.innerHTML = elem;
+    container.append(div);
+  });
+}
+
+loadTrending("action", 1);
+loadPopular("car", 1);
+
+// Input Search
+
+const form = document.querySelector("#user-input");
+const searchBar = document.querySelector("#search-bar");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  getData(search.value);
-  search.value = " ";
+  getData(searchBar.value);
+  searchBar.value = " ";
 });
 
-// Page Load Data
-async function pageDispaly() {
+async function getData(searchValue) {
   try {
-    const imgPath = `https://image.tmdb.org/t/p/w500`;
-    const url = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1`
+    const key = "78f34ce1";
+    const search = searchValue;
+    const res = await fetch(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${key}&t=${search}`
     );
-    const data = await url.json();
-    const page = data.results;
-
-    console.log(page);
-    
-
-    for (const card of page) {
-      const {
-        poster_path,
-        title,
-        overview: story,
-        popularity: rating,
-        release_date: date,
-      } = card;
-
-      let div = document.createElement("div");
-      let img = document.createElement("img");
-      let heading = document.createElement("h3");
-
-      heading.innerText = title;
-
-      img.setAttribute("src", `${imgPath + poster_path}`);
-      div.setAttribute("class", `card`);
-      heading.setAttribute("class", `sm-title`);
-
-      cards.append(div);
-      div.append(img);
-      div.append(heading);
-    }
+    const result = await res.json();
+    displayData(result);
   } catch (error) {
-    console.log(error);
+    console.error("Search Error");
   }
 }
-pageDispaly();
+function displayData(value) {
+  const trending = document.querySelector("#trending");
+  const popular = document.querySelector("#popular");
+  const searchboxInput = document.querySelector("#search-box-input");
 
-// Search data
-async function getData(value) {
-  try {
-    const apiKey = `221ad1f091a4e87b75ed4acb95dfdb32`;
-    const search = value;
-    const url = await fetch(
-      ` https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
-        search
-      )}`
-    );
-    const res = await url.json();
-    displayData(res);
-  } catch (error) {
-    console.log(error);
-  }
-}
-// Display data in page
-async function displayData(res) {
-  console.log(res);
-  const [data] = res.results;
-  const imgPath = `https://image.tmdb.org/t/p/w500`;
-  const {
-    poster_path,
-    title,
-    overview: story,
-    popularity: rating,
-    release_date: date,
-  } = data;
+  trending.innerHTML = "";
+  popular.innerHTML = "";
 
-  cards.innerHTML = " ";
-  let div1 = document.createElement("div");
-  let div = document.createElement("div");
-  let div2 = document.createElement("div");
-  let div3 = document.createElement("div");
-  let div4 = document.createElement("div");
-  let img = document.createElement("img");
-  let heading = document.createElement("h1");
-  let para = document.createElement("p");
-  let para2 = document.createElement("p");
-  let para3 = document.createElement("p");
-  let a = document.createElement("a");
-
-  heading.innerHTML = `Title: ${title}`;
-  a.innerText = `⬅️ Back`;
-  para3.innerHTML = `<b>Movie Details:</b> ${story}`;
-  para.innerHTML = `<b>Rating:</b> ${rating}`;
-  para2.innerHTML = `<b>Relese Date:</b> ${date}`;
-
-  img.setAttribute("src", `${imgPath + poster_path}`);
-  a.setAttribute("class", `home-btn`);
-  div1.setAttribute("class", `box-1`);
-  div3.setAttribute("class", `a-con`);
-  div.setAttribute("class", `box`);
-  div4.setAttribute("class", `box-3`);
-  div2.setAttribute("class", `box-2`);
-
-  a.addEventListener("click", () => {
-    cards.innerHTML = "";
-    pageDispaly();
-  });
-  cards.append(div1);
-  div1.prepend(div3);
-  div3.append(a);
-  div1.append(div);
-  div.prepend(div4);
-  div4.append(img);
-  div.append(div2);
-  div2.append(heading);
-  div2.append(para);
-  div2.append(para2);
-  div2.append(para3);
+  searchboxInput.innerHTML = `<div class="flex justify-between" >
+          <div
+            class="md:w-40 w-28 p-2.5 m-2.5 rounded-lg hover:bg-neutral-800 hover:text-white transition-all duration-300 dark:text-white text-black"
+          >
+            <img
+              src="${value.Poster}"
+              class="rounded-lg md:h-40 md:w-40"
+            />
+            <div class="mt-1 text-sm">
+              <h1 class="p-0.5 md:text-lg text-sm font-medium">${value.Title}</h1>
+              <h5 class="p-0.5 md:text-sm text-xs font-medium">Year : ${value.Year}</h5>
+            </div>
+          </div>
+          <div>
+            <a href="index.html" class="cursor-pointer">⬅️ Back</a>
+          </div>
+          </div>`;
 }
